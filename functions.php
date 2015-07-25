43dc23a3ef1f26c8f891a8f6fa1f54ef628399be
cepetrie <christinapetrie@hotmail.com>
2015-07-24 19:52:52 -0700

    Added sidebar functions to functions.php

--- functions.php
+++ functions.php
@@ -17,15 +17,39 @@ Version: 1.0
 register_nav_menus(array(
 'main-menu' => __( 'Main' ),
 ));
-//
 
-//register sidebar
-register_sidebar(array(
-	'before_widget' => '<div>',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+
+//Register sidebars
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+function my_register_sidebars() {
+
+	/* Register the primary sidebar. */
+	register_sidebar(
+		array(
+			'id' => 'primary',
+			'name' => __( 'Primary Sidebar' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">',
+			'after_title' => '</h3>'
+		)
+	);
+	
+	/* Register the 'secondary' sidebar. */
+	register_sidebar(
+		array(
+			'id' => 'secondary',
+			'name' => __( 'Secondary Sidebar' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">',
+			'after_title' => '</h3>'
+		)
+	);
+	
+		/* Repeat register_sidebar() code for additional sidebars. */
+}
 
 
 
