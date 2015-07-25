60f90714790b8b03d5a3b1bf24ee120c75c990ea
Mike Murphy <murph517@gmail.com>
2015-07-25 12:46:57 -0700

    Added additional html to index.php and page.php. Chenged current format

--- page.php
+++ page.php
@@ -1,26 +1,33 @@
+
 <?php get_header(); ?>
-
-<div id="main">                
+                
 <!-- START CONTENT -->
-<div id="content" class="page">
-    
-	<?php if (have_posts()) : ?>
-	<?php while (have_posts()) : the_post(); ?>
-
-		<h2><?php the_title(); ?></h2>
+<div id="main">  
+    <div id="content" class="page">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="post-<?php the_ID(); ?>">
+        <h2><?php the_title(); ?></h2>
+        <br />
 		<?php the_content(); ?>
- 
-	<?php endwhile; ?>
-	<?php endif; ?>
+        
+        <nav class="post-nav">            
+			<div class="alignleft"><?php previous_post_link('&laquo; %link', '%title', true) ?></div>
+			<div class="alignright"><?php next_post_link('%link &raquo;', '%title', true) ?></div>
+		</nav>
+        
+        <?php edit_post_link('Edit this entry.', '<p class="clear"><small>', '</small></p>'); ?>
+	</div>
+   
+	<?php endwhile; endif; ?>  
     
-</div><!-- END CONTENT -->
-
-<!-- START SIDEBAR -->
-
+</div>
+    
+<!-- END CONTENT -->
 <?php get_sidebar( 'secondary' ); ?>
 </div><!-- END MAIN -->
-<?php get_sidebar( 'primary' ); ?>
 
+<!-- PRIMARY SIDEBAR -->
+<?php get_sidebar('primary'); ?>
 <!-- END SIDEBAR -->
 
 <?php get_footer(); ?>
