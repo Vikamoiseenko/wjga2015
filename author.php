25561d1c98f9d4ad5a6365a83685d8fe007fb3be
Mike Murphy <murph517@gmail.com>
2015-08-07 20:21:19 -0700

    Adds Comment and Author pages. Fixes bugs on Index Page. Cleans up Functions doc. Adds styles for Blog Sidebar and Comment section.

--- author.php
+++ author.php
@@ -1,17 +1,34 @@
 <?php get_header(); ?>
-  
+
 <div id="main">
-<!-- START CONTENT -->
-<div id="content" class="index">
+<div id="content" class="author">
+
+    
+<!-- Create variable for the Creator of Post -->
+        <?php
+          if(isset($_GET['author_name'])) : $postAuthor = get_userdatabylogin($author_name);
+          else : $postAuthor = get_userdata(intval($author));
+          endif;
+        ?>
+<?php
+
+    if(isset($_GET['author_name'])) :
+        $postAuthor = get_userdatabylogin($author_name);
+    else :
+        $postAuthor = get_userdata(intval($author));
+    endif;
+    ?>
+    
+<!-- LOOP FOR POSTS BY AUTHOR-->
 <?php if (have_posts()) : ?>
+    
+		<h2>By: <?php the_author(); ?></h2>
 
     <?php while (have_posts()) : the_post(); ?>
 
     	<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 	
-		<p class="postmetadata">Posted <?php the_time('F jS, Y') ?> </p>
-	
-	
+		
         <p class="postmetadata">
 			<span class="cat">Posted <?php the_time('F jS, Y') ?> </span>
             <span class="cat"> in <?php the_category(', ') ?></span>
@@ -22,11 +39,14 @@
      	<?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
 
     <?php endwhile; ?>
-<nav class="post-nav">
-            <p class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></p>
-            <p class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></p>
-		</nav>
-<?php endif; ?>
+
+    
+	<?php else : ?>
+        <!--NO POST FOUND BY AUTHOR-->
+        <p>Sorry, we were unable to find posts by <?php echo $postAuthor->display_name; ?></p>
+    <?php endif; ?>
+<!-- End Loop -->
+
 </div><!-- END CONTENT -->
 
 
@@ -36,5 +56,6 @@
 
 <!-- START BLOG SIDEBAR -->
 <?php get_sidebar('blog'); ?>
-
+    
+    
 <?php get_footer(); ?>
