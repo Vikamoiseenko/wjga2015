62ec7a24ced885f1570223c24b3b0f1fe173a42d
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-08-14 14:21:02 -0700

    updated achive.php

--- archive.php
+++ archive.php
@@ -1,61 +1,70 @@
-<?php get_header(); ?>
+<?php get_header(); /*Template Name: archive.php*/?>
 
 <div id="main">                
 <!-- START CONTENT -->
 <div id="content" class="archive">
 
-    <?php if (have_posts()) : ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
- 	  <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
- 	  <?php /* If this is a category archive */ if (is_category()) { ?>
-		<h2 class="pagetitle">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
- 	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h2 class="pagetitle">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
- 	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
- 	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h2>
- 	  <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
-	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h2 class="pagetitle">Author Archive</h2>
- 	  <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h2 class="pagetitle">Blog Archives</h2>
- 	  <?php } ?>
+<header class="entry-header">
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+</header><!-- .entry-header -->
 
-		<?php while (have_posts()) : the_post(); ?>
-        <article class="post"> 
-        	
-            <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <p class="postmetadata">
-            <span>Posted on <?php the_time('F jS, Y') ?></span> in 
-            <span><?php the_category(', ') ?></span> by
-            <span><?php the_author_posts_link() ?> </span>
-        	</p>
-            
-        </article> 
+<div class="entry-content">
+  <?php the_content(); ?>
+
+  <?php if(is_active_sidebar('archives-left')) dynamic_sidebar('archives-left'); ?>
+  <?php if(is_active_sidebar('archives-right')) dynamic_sidebar('archives-right'); ?>
+  <div style="clear: both; margin-bottom: 30px;"></div><!-- clears the floating -->
+
+  <?php
+  $how_many_last_posts = intval(get_post_meta($post->ID, 'archived-posts-no', true));
+  if($how_many_last_posts > 200 || $how_many_last_posts < 2) $how_many_last_posts = 15;
+
+  $my_query = new WP_Query('post_type=post&nopaging=1');
+  if($my_query->have_posts()) {
+    echo '<h1 class="widget-title">Last '.$how_many_last_posts.' Posts <i class="fa fa-bullhorn" style="vertical-align: baseline;"></i></h1>&nbsp;';
+    echo '<div class="archives-latest-section"><ol>';
+    $counter = 1;
+    while($my_query->have_posts() && $counter <= $how_many_last_posts) {
+      $my_query->the_post();
+      ?>
+      <li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+      <?php
+      $counter++;
+    }
+    echo '</ol></div>';
+    wp_reset_postdata();
+  }
+  ?>
+
+  <h1 class="widget-title">Our Authors <i class="fa fa-user" style="vertical-align: baseline;"></i></h1>&nbsp;
+  <div class="archives-authors-section">
+    <ul>
+      <?php wp_list_authors('exclude_admin=0&optioncount=1'); ?>
+    </ul>
+  </div>
+
+  <h1 class="widget-title">By Month <i class="fa fa-calendar" style="vertical-align: baseline;"></i></h1>&nbsp;
+  <div class="archives-by-month-section">
+    <p><?php $variable = wp_get_archives( array( 'type' => 'monthly','show_post_count' => 'true') );
+
+$variable = str_replace( '(', '<span class="cat-count">', $variable);
+$variable = str_replace( ')', '</span>', $variable);
+echo $variable;    
     
-		<?php endwhile; ?>
+	
+    ?></p>
+    
+    
+  </div>
+</div><!-- .entry-content -->
 
-           <div class="alignleft"><?php previous_posts_link() ?></div>
-		   <div class="alignright"><?php next_posts_link(); ?></div>    
-        
-	<?php else :
+</article><!-- #post-## -->
+<?php endwhile; else: ?>
+<?php endif; ?>
 
-		if ( is_category() ) { // If this is a category archive
-			printf("<h2 class='center'>Sorry, but there aren't any posts in the %s category yet.</h2>", single_cat_title('',false));
-		} else if ( is_date() ) { // If this is a date archive
-			echo("<h2>Sorry, but there aren't any posts with this date.</h2>");
-		} else if ( is_author() ) { // If this is a category archive
-			$userdata = get_userdatabylogin(get_query_var('author_name'));
-			printf("<h2 class='center'>Sorry, but there aren't any posts by %s yet.</h2>", $userdata->display_name);
-		} else {
-			echo("<h2 class='center'>No posts found.</h2>");
-		}
-		get_search_form();
-
-	endif;
-?>
 
 </div><!-- END CONTENT -->
 
@@ -63,7 +72,7 @@
 
 <?php get_sidebar( 'secondary' ); ?>
 </div><!-- END MAIN -->
-<?php get_sidebar( 'blog' ); ?>
+<?php //get_sidebar( 'archive' ); ?>
 
 <!-- END SIDEBAR -->
 
